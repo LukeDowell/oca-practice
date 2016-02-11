8955581456f75e911732850cc1a130fdb6d830d1
Luke Dowell <lukedowell@gmail.com>
2016-02-10 19:34:14 -0600

    Initial commit

---
+++ src/test/java/org/lukedowell/oca/assessment/Assessment.java
@@ -0,0 +1,20 @@
+package org.lukedowell.oca.assessment;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by lukedowell on 2/10/16.
+ */
+public class Assessment {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void one() {
+        assert true;
+    }
+}
